d4737d28dfc526dcfeaae3f4e0bed6c2ac6e8119
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2017-05-11 13:12:32 -0300

    classe Nodo, com operadores sobrecarregados.

--- ex_pratico1/nodo.h
+++ ex_pratico1/nodo.h
@@ -1,4 +1,49 @@
+using namespace std;
+
 struct Nodo {
 	Nodo *filhoEsquerda, *filhoDireita;
+	Nodo *raiz;
 	float probabilidade;
+	char simbolo;
+	string codigo;
+
+	Nodo(Nodo* esq, Nodo* dir, Nodo* raiz, float probabilidade, char simbolo){
+		this->filhoEsquerda = esq; 
+		this->filhoDireita = dir;
+		this->raiz = raiz;
+		this->probabilidade = probabilidade;
+		this->simbolo = simbolo;
+	}
+
+	Nodo(){
+		this->filhoEsquerda = NULL;
+		this->filhoDireita = NULL;
+		this->raiz = NULL;
+		this->probabilidade = 0;
+		this->codigo = "";
+	}
+
+	bool operator==(const Nodo &n) const {
+        return this->probabilidade == n.probabilidade;
+    }
+
+    bool operator!=(const Nodo &n) const {
+        return (this->probabilidade != n.probabilidade);
+    }
+
+    bool operator<(const Nodo &n) const {
+        return (this->probabilidade < n.probabilidade);
+    }
+
+    bool operator<=(const Nodo &n) const {
+        return (this->probabilidade <= n.probabilidade);
+    }
+
+    bool operator>(const Nodo &n) const {
+        return (this->probabilidade > n.probabilidade);
+    }
+
+    bool operator>=(const Nodo &n) const {
+        return (this->probabilidade >= n.probabilidade);
+    }
 };
